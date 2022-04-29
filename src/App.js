1ab5f55113ead7be7012368b722a65eb4dba8f88
Adarshcodes <adarsh.lrnr@gmail.com>
2022-04-29 23:52:27 +0530

    adding a card component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Cards from "./components/Cards";
 import "./assets/sass/main.css";
 
 function App() {
 	return (
 		<main className="app">
 			<header className="app-header"></header>
+			<Cards />
 		</main>
 	);
 }
