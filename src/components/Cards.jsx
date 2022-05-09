01dbfe3380518dfb9469d765a62bd69a2efcadcb
Adarshcodes <adarsh.lrnr@gmail.com>
2022-05-09 22:45:31 +0530

    making card into class component

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
-export default function Cards() {
-	return <div>This is the card component</div>;
+class Cards extends React.Component {
+	render() {
+		return <div>This is the card component</div>;
+	}
 }
+
+export default Cards;
