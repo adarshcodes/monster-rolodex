c280e45f605e51e2467b6ba6c13a256e3203beba
Adarshcodes <adarsh.lrnr@gmail.com>
2022-05-14 17:03:22 +0530

    converted the app into functional component

--- src/App.js
+++ src/App.js
@@ -46,80 +46,21 @@ import "./assets/sass/main.css";
 // }
 
 function App() {
-	const [monstersData, setMonsters] = useState({
-		monsters: [
-			{
-				id: 1,
-				name: "Leanne Graham",
-				username: "Bret",
-				email: "Sincere@april.biz",
-				address: {
-					street: "Kulas Light",
-					suite: "Apt. 556",
-					city: "Gwenborough",
-					zipcode: "92998-3874",
-					geo: {
-						lat: "-37.3159",
-						lng: "81.1496",
-					},
-				},
-				phone: "1-770-736-8031 x56442",
-				website: "hildegard.org",
-				company: {
-					name: "Romaguera-Crona",
-					catchPhrase: "Multi-layered client-server neural-net",
-					bs: "harness real-time e-markets",
-				},
-			},
-			{
-				id: 2,
-				name: "Ervin Howell",
-				username: "Antonette",
-				email: "Shanna@melissa.tv",
-				address: {
-					street: "Victor Plains",
-					suite: "Suite 879",
-					city: "Wisokyburgh",
-					zipcode: "90566-7771",
-					geo: {
-						lat: "-43.9509",
-						lng: "-34.4618",
-					},
-				},
-				phone: "010-692-6593 x09125",
-				website: "anastasia.net",
-				company: {
-					name: "Deckow-Crist",
-					catchPhrase: "Proactive didactic contingency",
-					bs: "synergize scalable supply-chains",
-				},
-			},
-		],
-		searchField: "",
-	});
+	const [searchField, setSearchField] = useState("");
 
-	// useEffect(() => {
-	// 	fetch("https://jsonplaceholder.typicode.com/users")
-	// 		.then((res) => res.json())
-	// 		.then((user) =>
-	// 			setMonsters((prevMonsters) => {
-	// 				return {
-	// 					monsters: user,
-	// 				};
-	// 			})
-	// 		);
-	// }, []);
+	const [monsters, setMonsters] = useState([]);
+	console.log(searchField, monsters);
+
+	useEffect(() => {
+		fetch("https://jsonplaceholder.typicode.com/users")
+			.then((res) => res.json())
+			.then((user) => setMonsters(user));
+	}, []);
 
 	const onSearchChange = (event) => {
-		const searchField = event.target.value.toLocaleLowerCase();
-		setMonsters((prevSearch) => {
-			return {
-				prevSearch: searchField,
-			};
-		});
+		const searchFieldString = event.target.value.toLocaleLowerCase();
+		setSearchField(searchFieldString);
 	};
-	// Destructuring the state in to their name to make it simple
-	const { monsters, searchField } = monstersData;
 
 	// Filtering out the serched mosters form monsters state
 	const filteredMonsters = monsters.filter((monster) => {
--- src/components/search-field/SearchField.component.jsx
+++ src/components/search-field/SearchField.component.jsx
@@ -1,29 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 
-// class SearchField extends Component {
-// 	render() {
-// 		const { onChangeHandler } = this.props;
-// 		return (
-// 			<input
-// 				className="search-box"
-// 				type="search"
-// 				placeholder="Search Monster"
-// 				onChange={onChangeHandler}
-// 			/>
-// 		);
-// 	}
-// }
-
-function SearchField(props) {
-	const { onChangeHandler } = props;
-	return (
-		<input
-			className="search-box"
-			type="search"
-			placeholder="Search Monster"
-			onChange={onChangeHandler}
-		/>
-	);
+class SearchField extends Component {
+	render() {
+		const { onChangeHandler } = this.props;
+		return (
+			<input
+				className="search-box"
+				type="search"
+				placeholder="Search Monster"
+				onChange={onChangeHandler}
+			/>
+		);
+	}
 }
+
+// function SearchField(props) {
+// 	const { onChangeHandler } = props;
+// 	return (
+// 		<input
+// 			className="search-box"
+// 			type="search"
+// 			placeholder="Search Monster"
+// 			onChange={onChangeHandler}
+// 		/>
+// 	);
+// }
 
 export default SearchField;
